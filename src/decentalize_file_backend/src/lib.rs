e1671ce6d90c79814417caddf1c01d8686315ddc
John <john.oladeji@slsbank.com>
2024-02-24 07:04:07 +0100

    implemented  filestorage for decentralized file storage

--- src/decentalize_file_backend/src/lib.rs
+++ src/decentalize_file_backend/src/lib.rs
@@ -1,10 +1,17 @@
+//use ic_cdk_macros::export;
+use ic_cdk::export;
+use ic_cdk::export::candid::{CandidType, Deserialize, Func, Principal};
+use ic_cdk::print;
+
 
 // Define data structures for files and directories
+#[derive(Clone, CandidType, Deserialize, Serialize)]
 struct File {
     name: String,
     content: Vec<u8>,
 }
 
+#[derive(Clone, CandidType, Deserialize, Serialize)]
 struct Directory {
     name: String,
     files: Vec<File>,
@@ -12,3 +19,64 @@ struct Directory {
 }
 
 // Define a trait for interacting with the file storage system
+#[export]
+trait FileStorage {
+    fn upload_file(&mut self, directory_path: String, file_name: String, content: Vec<u8>) -> Result<(), String>;
+    fn download_file(&self, file_path: String) -> Result<Vec<u8>, String>;
+    fn create_directory(&mut self, parent_directory_path: String, directory_name: String) -> Result<(), String>;
+}
+
+// Implement the FileStorage trait for a decentralized file storage canister
+struct DecentralizedFileStorage {
+    root_directory: Directory,
+}
+
+impl FileStorage for DecentralizedFileStorage {
+    fn upload_file(&mut self, directory_path: String, file_name: String, content: Vec<u8>) -> Result<(), String> {
+        // Find the directory specified by directory_path
+        // If directory doesn't exist, return an error
+        
+        // Create a new File instance with the provided content
+        let new_file = File {
+            name: file_name.clone(),
+            content,
+        };
+
+        // Add the new file to the directory's files vector
+        // Return success
+        self.root_directory.files.push(new_file);
+        Ok(())
+    }
+
+    fn download_file(&self, file_path: String) -> Result<Vec<u8>, String> {
+        // Find the file specified by file_path
+        // If file doesn't exist, return an error
+        
+        // Return the content of the file
+        for file in &self.root_directory.files {
+            if file.name == file_path {
+                return Ok(file.content.clone());
+            }
+        }
+        Err("File not found".to_string())
+    }
+
+    fn create_directory(&mut self, parent_directory_path: String, directory_name: String) -> Result<(), String> {
+        // Find the parent directory specified by parent_directory_path
+        // If parent directory doesn't exist, return an error
+        
+        // Create a new Directory instance
+        let new_directory = Directory {
+            name: directory_name.clone(),
+            files: Vec::new(),
+            subdirectories: Vec::new(),
+        };
+
+        // Add the new directory to the parent directory's subdirectories vector
+        // Return success
+        self.root_directory.subdirectories.push(new_directory);
+        Ok(())
+    }
+
+    // Implement other methods such as listing files in a directory, deleting files/directories, etc.
+}
